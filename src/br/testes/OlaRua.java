9a2fb023c68870699e00b6f9a183c093853695f2
Alex Rodrigues <alferodrigues@gmail.com>
2022-01-23 19:13:55 -0300

    Branch conflito dev2

--- src/br/testes/OlaRua.java
+++ src/br/testes/OlaRua.java
@@ -5,6 +5,7 @@ public class OlaRua {
 	public static void main(String[] args) {
 		System.out.println("Ola Rua!!");
 		System.out.println("Oi Rua!!");
+		System.out.println("Branch conflito dev2 aa");
 	}
 
 }
