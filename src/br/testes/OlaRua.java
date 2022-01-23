feef1e0867c6535a17f18a1949d9416d96b2132f
Alex Rodrigues <alferodrigues@gmail.com>
2022-01-23 19:17:26 -0300

    Alteração

--- src/br/testes/OlaRua.java
+++ src/br/testes/OlaRua.java
@@ -5,7 +5,7 @@ public class OlaRua {
 	public static void main(String[] args) {
 		System.out.println("Ola Rua!!");
 		System.out.println("Oi Rua!!");
-		System.out.println("Branch main dev2!!");
+		System.out.println("Branch main dev2 bb!!");
 	}
 
 }
