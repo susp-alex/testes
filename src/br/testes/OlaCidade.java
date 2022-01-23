d744befe782430228028094a5c6600cbc3dc74a3
Alex Rodrigues <alferodrigues@gmail.com>
2022-01-23 20:48:17 -0300

    branch_teste_stash alteração 1 dev2

--- src/br/testes/OlaCidade.java
+++ src/br/testes/OlaCidade.java
@@ -4,6 +4,7 @@ public class OlaCidade {
 
 	public static void main(String[] args) {
 		System.out.println("Ola Cidade!! dev1 dev2");
+		System.out.println("branch_teste_stash alteração 1 dev2");
 	}
 
 }
