837861713ef43928f19e706b01a4b7160461daf6
Alex Rodrigues <alferodrigues@gmail.com>
2022-01-23 20:38:16 -0300

    Branch main alteração predio 1 dev1

--- src/br/testes/OlaPredio.java
+++ src/br/testes/OlaPredio.java
@@ -5,6 +5,7 @@ public class OlaPredio {
 	public static void main(String[] args) {
 		System.out.println("Ola Predio!! dev1");
 		System.out.println("Oi Predio!! dev2");
+		System.out.println("Branch main alteração predio 1 dev1");
 	}
 	
 }
