78cbba1620b37e0fe327157d2a7015c768b236ef
Alex Rodrigues <alferodrigues@gmail.com>
2022-01-23 20:51:06 -0300

    main alteração 1 dev2 e main alteração 2 dev2

--- src/br/testes/OlaCidade.java
+++ src/br/testes/OlaCidade.java
@@ -5,6 +5,8 @@ public class OlaCidade {
 	public static void main(String[] args) {
 		System.out.println("Ola Cidade!! dev1 dev2");
 		System.out.println("branch_teste_stash alteração 1 dev2");
+		System.out.println("main alteração 1 dev2");
+		System.out.println("main alteração 2 dev2");
 	}
 
 }
