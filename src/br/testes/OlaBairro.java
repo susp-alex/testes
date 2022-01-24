9eb97ebb2cc122e1aad4c4e9e3710969981f90d8
Alex Rodrigues <alferodrigues@gmail.com>
2022-01-23 23:19:36 -0300

    main alteração 1 dev1

--- src/br/testes/OlaBairro.java
+++ src/br/testes/OlaBairro.java
@@ -3,6 +3,7 @@ package br.testes;
 public class OlaBairro {
 
 	public static void main(String[] args) {
+		System.out.println("main alteração 1 dev1!!");
 		System.out.println("Ola Bairroooo!!");
 	}
 
